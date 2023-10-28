f070e78bf0e7dc2afb105baa85b41dd7b071cec2
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-10-27 17:51:12 -0700

    change to winsert image

--- src/plugins/toolbar/components/InsertImage.tsx
+++ src/plugins/toolbar/components/InsertImage.tsx
@@ -16,7 +16,7 @@ export const InsertImage = React.forwardRef<HTMLButtonElement, Record<string, ne
 
   return (
     <RadixToolbar.Button className={styles.toolbarButton} ref={forwardedRef} disabled={readOnly} onClick={() => openNewImageDialog(true)}>
-      <TooltipWrap title="Insert image">
+      <TooltipWrap title="WInsert image">
         <AddPhotoIcon />
       </TooltipWrap>
     </RadixToolbar.Button>
